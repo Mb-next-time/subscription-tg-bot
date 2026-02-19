9e3774a9c4f35eaba074a602f2b455f9aa0062dc
Mb Next Time <sombrero.oval@gmail.com>
2026-02-19 23:16:13 +0700

    Registration of user in database

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,14 +1,23 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
+from sqlalchemy import insert, select
 
 from bot.keyboards.main_menu import main_menu
+from database.config import get_database_session
+from database.models import User
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def start_handler(message: Message):
+    async with get_database_session() as database_session:
+        telegram_id = message.from_user.id
+        user_exists = (await database_session.execute(select(User).where(User.telegram_id == telegram_id))).scalar_one_or_none()
+        if user_exists is None:
+            statement = insert(User).values(telegram_id=telegram_id)
+            await database_session.execute(statement)
     await message.answer(
         "Добро пожаловать в MemeBot 😎\n\n"
         "7 дней бесплатно, потом подписка.",
