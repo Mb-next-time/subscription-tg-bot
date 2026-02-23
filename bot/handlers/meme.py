3f574b4fce1b46090ebea974ee527dd846692ebd
Mb Next Time <sombrero.oval@gmail.com>
2026-02-23 17:42:47 +0700

    Fixed condition for reset daily restriction

--- bot/handlers/meme.py
+++ bot/handlers/meme.py
@@ -18,12 +18,12 @@ router = Router()
 
 
 @router.message(lambda message: message.text == ButtonText.GIVE_MEME.value)
-async def show_meme(message: Message):
+async def show_meme(message: Message) -> None:
     async with get_database_session() as database_session:
         meme_file = random.choice(memes)
         telegram_id = message.from_user.id
         user: User = (await database_session.execute(select(User).where(User.telegram_id == telegram_id))).scalar_one_or_none()
-        time_is_passed = (datetime.now(timezone.utc) - user.timestamp_first_count).seconds
+        time_is_passed = (datetime.now(timezone.utc) - user.timestamp_first_count).total_seconds()
         if time_is_passed >= TIME_FOR_DAILY_RESET:
             user.daily_count = 0
         if user.daily_count >= DAILY_MAX_WITHOUT_SUB:
